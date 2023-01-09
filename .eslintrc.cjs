3025217a78ba33a8d917e68a354968d4ed734da5
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-01-10 02:50:56 +0900

    🆙 storybook 雑に入れる

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'next/core-web-vitals',
+    'plugin:storybook/recommended',
     'prettier',
   ],
   overrides: [],
