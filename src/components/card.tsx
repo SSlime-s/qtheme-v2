d974f58084498ec77604478b238c82ad4787d452
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-01-11 00:02:01 +0900

    ✨ グラスモーフィズムのカード用のコンポーネント作る

---
+++ src/components/card.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+
+export const GlassmorphismCard = styled.div`
+  border-radius: 20px;
+  background: radial-gradient(
+    0% 0%,
+    rgba(255, 255, 255, 0.3) 0%,
+    rgba(255, 255, 255, 0.1) 100%
+  );
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(10px);
+  overflow: hidden;
+`
