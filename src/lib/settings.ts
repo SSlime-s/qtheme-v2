cba6ceac01665dea4594e35561fd32d00bf43bbf
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-01-12 14:59:42 +0900

    👕 fmt

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest } from "next"
+import { NextApiRequest } from 'next'
 
 export const extractUserName = (req: NextApiRequest) => {
   const userId = req.headers['x-showcase-user'] as string | undefined
