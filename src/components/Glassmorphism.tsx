5ce0dc26e34e4bb5d836ff71305c00414b82cf10
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-01-14 23:33:47 +0900

    🎨 グラスモーフィズムの背景色を濃くする

--- src/components/Glassmorphism.tsx
+++ src/components/Glassmorphism.tsx
@@ -4,10 +4,10 @@ export const GlassmorphismStyle = css`
   border-radius: 20px;
   background: radial-gradient(
     ellipse at 0% 0%,
-    rgba(255, 255, 255, 0.3) 0%,
-    rgba(255, 255, 255, 0.1) 100%
+    rgba(255, 255, 255, 0.5) 0%,
+    rgba(255, 255, 255, 0.4) 100%
   );
-  border: 1px solid rgba(255, 255, 255, 0.3);
+  border: 1px solid rgba(255, 255, 255, 0.5);
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.25);
   backdrop-filter: blur(10px);
   overflow: hidden;
