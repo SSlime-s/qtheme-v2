a1ec9011456d6248fccab7d0d606a74b78ac5e32
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-01-14 23:03:42 +0900

    🎨 border つけ忘れてた

--- src/components/Glassmorphism.tsx
+++ src/components/Glassmorphism.tsx
@@ -7,6 +7,7 @@ export const GlassmorphismStyle = css`
     rgba(255, 255, 255, 0.3) 0%,
     rgba(255, 255, 255, 0.1) 100%
   );
+  border: 1px solid rgba(255, 255, 255, 0.3);
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.25);
   backdrop-filter: blur(10px);
   overflow: hidden;
