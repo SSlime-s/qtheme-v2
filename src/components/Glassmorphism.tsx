5be776a26bd77830e7399dda409904383afc525f
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-01-14 23:05:33 +0900

    😱radial-gradient ellipse がないと想定した見た目になってなかった

--- src/components/Glassmorphism.tsx
+++ src/components/Glassmorphism.tsx
@@ -3,7 +3,7 @@ import { css } from '@emotion/react'
 export const GlassmorphismStyle = css`
   border-radius: 20px;
   background: radial-gradient(
-    0% 0%,
+    ellipse at 0% 0%,
     rgba(255, 255, 255, 0.3) 0%,
     rgba(255, 255, 255, 0.1) 100%
   );
