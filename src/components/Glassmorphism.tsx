8ec31ce73daaac78002679d2d6c4d663b3e534a1
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-01-15 02:33:32 +0900

    ♻️ グラスモーフィズムのカードの色変えれるようにする

--- src/components/Glassmorphism.tsx
+++ src/components/Glassmorphism.tsx
@@ -1,14 +1,19 @@
 import { css } from '@emotion/react'
 
-export const GlassmorphismStyle = css`
+export const ColoredGlassmorphismStyle = (
+  from: string,
+  to: string = from,
+  border: string = from
+) => css`
   border-radius: 20px;
-  background: radial-gradient(
-    ellipse at 0% 0%,
-    rgba(255, 255, 255, 0.5) 0%,
-    rgba(255, 255, 255, 0.4) 100%
-  );
-  border: 1px solid rgba(255, 255, 255, 0.5);
+  background: radial-gradient(ellipse at 0% 0%, ${from} 0%, ${to} 100%);
+  border: 1px solid ${border};
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.25);
   backdrop-filter: blur(10px);
   overflow: hidden;
 `
+export const GlassmorphismStyle = ColoredGlassmorphismStyle(
+  'rgba(255, 255, 255, 0.5)',
+  'rgba(255, 255, 255, 0.4)',
+  'rgba(255, 255, 255, 0.5)'
+)
