d52ed11c8fd9c5e4ac742a8b7a4d8bbf8e0dafe3
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-01-15 22:55:30 +0900

    ✨ google font 入れる
    .mono をつけたら等幅フォントになる

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,31 @@
 import '@/styles/globals.css'
 import '@/styles/reset.css'
 import type { AppProps } from 'next/app'
+import { Roboto, Roboto_Mono } from '@next/font/google'
+
+export const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
+
+export const robotoMono = Roboto_Mono({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+
+        .mono {
+          font-family: ${robotoMono.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
