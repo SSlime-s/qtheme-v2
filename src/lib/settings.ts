26947eb5a7b1af8e8f4f6e727a0ad223002a4251
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-01-16 14:36:24 +0900

    🗑️ extractUser が重複してた

--- src/lib/settings.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest } from 'next'
-
-export const extractUserName = (req: NextApiRequest) => {
-  const userId = req.headers['x-showcase-user'] as string | undefined
-  return userId
-}
