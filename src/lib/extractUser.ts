8df65a7b4c1016e9969c399e972d5d3d3fa90e7b
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-01-20 22:33:09 +0900

    🐛 extractUser の挙動がまだだめだった

--- src/lib/extractUser.ts
+++ src/lib/extractUser.ts
@@ -1,6 +1,18 @@
 import { IncomingMessage } from 'http'
+import { NextRequest } from 'next/server'
 
-export const extractShowcaseUser = (req: IncomingMessage) => {
-  const userId = req.headers['x-showcase-user'] as string | undefined
-  return userId === '-' ? undefined : userId
+const showcaseUserKey = 'x-showcase-user'
+export const extractShowcaseUser = (req: IncomingMessage | NextRequest) => {
+  let userId: string | undefined
+  if (req instanceof NextRequest) {
+    userId = req.headers.get(showcaseUserKey) ?? undefined
+  } else {
+    const unsafeUserId = req.headers[showcaseUserKey]
+    if (Array.isArray(unsafeUserId)) {
+      userId = unsafeUserId.join('')
+    } else {
+      userId = unsafeUserId
+    }
+  }
+  return userId === '-' ? undefined : userId ?? undefined
 }
