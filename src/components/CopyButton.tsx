21b15d9349885dec4f0cb1d4754601948b7bb136
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-01-30 16:47:30 +0900

    ✨ コピー用のボタンのコンポーネント追加

---
+++ src/components/CopyButton.tsx
@@ -0,0 +1,60 @@
+import styled from '@emotion/styled'
+import { useCallback, useState } from 'react'
+import { BsClipboard, BsClipboardCheck, BsClipboardX } from 'react-icons/bs'
+
+interface Props {
+  text: string
+  onClick?: () => void
+  onCopy?: () => void
+  onCopyError?: () => void
+}
+type CopyState = 'default' | 'copied' | 'error'
+export const CopyButton: React.FC<
+  Props & React.HTMLAttributes<HTMLButtonElement>
+> = ({ text, onClick, onCopy, onCopyError, ...props }) => {
+  const [status, setStatus] = useState<CopyState>('default')
+
+  const copy = useCallback(async () => {
+    try {
+      await navigator.clipboard.writeText(text)
+      setStatus('copied')
+      setTimeout(() => setStatus('default'), 700)
+      onCopy?.()
+    } catch (e) {
+      setStatus('error')
+      setTimeout(() => setStatus('default'), 700)
+      onCopyError?.()
+    }
+  }, [text, onCopy, onCopyError])
+
+  const handleClick = useCallback(() => {
+    onClick?.()
+    copy()
+  }, [copy, onClick])
+
+  return (
+    <Button onClick={handleClick} status={status} {...props}>
+      {status === 'default' ? (
+        <BsClipboard />
+      ) : status === 'copied' ? (
+        <BsClipboardCheck />
+      ) : (
+        <BsClipboardX />
+      )}
+    </Button>
+  )
+}
+const Button = styled.button<{
+  status: CopyState
+}>`
+  color: ${({ theme, status }) =>
+    status === 'default'
+      ? theme.theme.basic.accent.primary.default
+      : status === 'copied'
+      ? theme.theme.basic.accent.online.default
+      : theme.theme.basic.accent.error.default};
+  transition: all 0.2s ease-out;
+  cursor: pointer;
+  display: grid;
+  place-items: center;
+`
