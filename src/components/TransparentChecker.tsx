c49bbd1e6658b2359488e2abd2d003471d36f7b0
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-02-11 02:17:59 +0900

    ✨ 透明を表わす市松模様のスタイル定義

---
+++ src/components/TransparentChecker.tsx
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react'
+
+export const TransparentCheckerStyle = css`
+  background: rgb(204, 204, 204);
+  background-image: linear-gradient(
+      45deg,
+      rgb(255, 255, 255) 25%,
+      transparent 0
+    ),
+    linear-gradient(45deg, transparent 75%, rgb(255, 255, 255) 0),
+    linear-gradient(45deg, rgb(255, 255, 255) 25%, transparent 0),
+    linear-gradient(45deg, transparent 75%, rgb(255, 255, 255) 0);
+  background-size: 16px 16px;
+  background-position: 0 0, 8px 8px, 8px 8px, 16px 16px;
+`
