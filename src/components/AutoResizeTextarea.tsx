1d3c635c7aaf6bc05c217bc74c3740f8b0ec0e2c
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-02-15 02:38:55 +0900

    ✨ 自動伸縮するテキストエリア追加

---
+++ src/components/AutoResizeTextarea.tsx
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled'
+import { ComponentProps, useRef, useState, useCallback, useEffect } from 'react'
+
+type Props = ComponentProps<'textarea'>
+export const AutoResizeTextarea: React.FC<Props> = ({ onChange, ...props }) => {
+  const ref = useRef<HTMLTextAreaElement>(null)
+  const [height, setHeight] = useState<number | null>(null)
+  const [dummyValue, setDummyValue] = useState(props.value ?? '')
+
+  const onChangeHandler = useCallback(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      onChange?.(e)
+      setDummyValue(e.target.value)
+    },
+    [onChange]
+  )
+  useEffect(() => {
+    if (ref.current === null) {
+      return
+    }
+    const { scrollHeight } = ref.current
+    setHeight(scrollHeight)
+  }, [dummyValue])
+
+  return (
+    <>
+      <Textarea height={height} onChange={onChangeHandler} {...props} />
+      <Dummy
+        ref={ref}
+        value={dummyValue}
+        className={props.className}
+        aria-hidden='true'
+      />
+    </>
+  )
+}
+const Textarea = styled.textarea<{ height: number | null }>`
+  height: ${({ height }) => (height === null ? 'auto' : `${height}px`)};
+  resize: none;
+  overflow: hidden;
+`
+const Dummy = styled.textarea`
+  position: absolute !important;
+  visibility: hidden;
+  height: auto;
+  overflow: hidden;
+`
