75d605db39739d59d51d8118f73e7d6b63580522
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-02-23 23:42:12 +0900

    ♻️ BasicSelectors の切り分け

---
+++ src/pages/edit/BasicSelectors.tsx
@@ -0,0 +1,94 @@
+import { resolveTheme } from "@/lib/theme"
+import { Theme } from "@/model/theme"
+import styled from "@emotion/styled"
+import { useCallback, useMemo, useState } from "react"
+import { useFormContext, useWatch } from "react-hook-form"
+import { ColorSelector } from "./ColorSelector"
+import { Form } from "./index.page"
+
+const BasicKeys = {
+  accent: ['primary', 'notification', 'online', 'error', 'focus'] as const,
+  background: ['primary', 'secondary', 'tertiary'] as const,
+  ui: ['primary', 'secondary', 'tertiary'] as const,
+  text: ['primary', 'secondary'] as const,
+} as const
+const BasicKeysKeys = [
+  'accent',
+  'background',
+  'ui',
+  'text',
+] as const satisfies ReadonlyArray<keyof typeof BasicKeys>
+export const BasicSelectors: React.FC = () => {
+  const { control, setValue, getValues } = useFormContext<Form>()
+  const theme = useWatch({ control, name: 'theme' })
+  const basicTheme = useMemo(() => {
+    return theme.basic
+  }, [theme])
+  const resolvedTheme = useMemo(() => {
+    return resolveTheme(theme)
+  }, [theme])
+
+  const setBasicTheme = useCallback(
+    (newTheme: Partial<Theme['basic']>) => {
+      const prev = getValues('theme')
+      setValue(
+        'theme',
+        { ...prev, basic: { ...prev.basic, ...newTheme } },
+        {
+          shouldDirty: true,
+        }
+      )
+    },
+    [getValues, setValue]
+  )
+
+  const [expanded, setExpanded] = useState<string>()
+
+  return (
+    <div>
+      {BasicKeysKeys.map(key1 => (
+        <SelectorGroup key={key1}>
+          <SelectorGroupLabel>{key1}</SelectorGroupLabel>
+          {BasicKeys[key1].map(key2 => (
+            <Selector key={key2}>
+              <SelectorLabel>{key2}</SelectorLabel>
+              <ColorSelector
+                // @ts-expect-error: TODO: key の chain をうまく認識できてない
+                value={resolvedTheme.basic[key1][key2].default}
+                onChange={color => {
+                  setBasicTheme({
+                    [key1]: {
+                      ...basicTheme[key1],
+                      [key2]: color,
+                    },
+                  })
+                }}
+                isExpanded={expanded === `${key1}.${key2}`}
+                setExpanded={v =>
+                  setExpanded(v ? `${key1}.${key2}` : undefined)
+                }
+              />
+            </Selector>
+          ))}
+        </SelectorGroup>
+      ))}
+    </div>
+  )
+}
+const SelectorGroup = styled.div`
+  margin-bottom: 16px;
+`
+const SelectorGroupLabel = styled.div`
+  font-weight: bold;
+  color: #111;
+  margin-bottom: 8px;
+  letter-spacing: 0.0625em;
+`
+const Selector = styled.div`
+  margin-bottom: 8px;
+  padding-left: 8px;
+`
+const SelectorLabel = styled.div`
+  color: #333;
+  margin-bottom: 4px;
+`
--- src/pages/edit/index.page.tsx
+++ src/pages/edit/index.page.tsx
@@ -15,9 +15,7 @@ import { extractShowcaseUser } from '@/lib/extractUser'
 import { useCallback, useEffect, useId, useMemo, useRef, useState } from 'react'
 import { resolveTheme } from '@/lib/theme'
 import { SmallPreview } from '@/components/preview'
-import {
-  GlassmorphismStyle,
-} from '@/components/Glassmorphism'
+import { GlassmorphismStyle } from '@/components/Glassmorphism'
 import { useRouter } from 'next/router'
 import { useThemeList } from '@/lib/theme/hooks'
 import React from 'react'
@@ -30,6 +28,7 @@ import { AutoResizeTextarea } from '@/components/AutoResizeTextarea'
 import { SyncControls } from './SyncControls'
 import { ColorSelector } from './ColorSelector'
 import { AdvancedSelectors } from './AdvancedSelectors'
+import { BasicSelectors } from './BasicSelectors'
 
 export const getServerSideProps = async ({
   req,
@@ -414,90 +413,3 @@ const Preview: React.FC<{ userId: string | null }> = ({ userId }) => {
     </div>
   )
 }
-
-const BasicKeys = {
-  accent: ['primary', 'notification', 'online', 'error', 'focus'] as const,
-  background: ['primary', 'secondary', 'tertiary'] as const,
-  ui: ['primary', 'secondary', 'tertiary'] as const,
-  text: ['primary', 'secondary'] as const,
-} as const
-const BasicKeysKeys = [
-  'accent',
-  'background',
-  'ui',
-  'text',
-] as const satisfies ReadonlyArray<keyof typeof BasicKeys>
-const BasicSelectors: React.FC = () => {
-  const { control, setValue, getValues } = useFormContext<Form>()
-  const theme = useWatch({ control, name: 'theme' })
-  const basicTheme = useMemo(() => {
-    return theme.basic
-  }, [theme])
-  const resolvedTheme = useMemo(() => {
-    return resolveTheme(theme)
-  }, [theme])
-
-  const setBasicTheme = useCallback(
-    (newTheme: Partial<Theme['basic']>) => {
-      const prev = getValues('theme')
-      setValue(
-        'theme',
-        { ...prev, basic: { ...prev.basic, ...newTheme } },
-        {
-          shouldDirty: true,
-        }
-      )
-    },
-    [getValues, setValue]
-  )
-
-  const [expanded, setExpanded] = useState<string>()
-
-  return (
-    <div>
-      {BasicKeysKeys.map(key1 => (
-        <SelectorGroup key={key1}>
-          <SelectorGroupLabel>{key1}</SelectorGroupLabel>
-          {BasicKeys[key1].map(key2 => (
-            <Selector key={key2}>
-              <SelectorLabel>{key2}</SelectorLabel>
-              <ColorSelector
-                // @ts-expect-error: TODO: key の chain をうまく認識できてない
-                value={resolvedTheme.basic[key1][key2].default}
-                onChange={color => {
-                  setBasicTheme({
-                    [key1]: {
-                      ...basicTheme[key1],
-                      [key2]: color,
-                    },
-                  })
-                }}
-                isExpanded={expanded === `${key1}.${key2}`}
-                setExpanded={v =>
-                  setExpanded(v ? `${key1}.${key2}` : undefined)
-                }
-              />
-            </Selector>
-          ))}
-        </SelectorGroup>
-      ))}
-    </div>
-  )
-}
-const SelectorGroup = styled.div`
-  margin-bottom: 16px;
-`
-const SelectorGroupLabel = styled.div`
-  font-weight: bold;
-  color: #111;
-  margin-bottom: 8px;
-  letter-spacing: 0.0625em;
-`
-const Selector = styled.div`
-  margin-bottom: 8px;
-  padding-left: 8px;
-`
-const SelectorLabel = styled.div`
-  color: #333;
-  margin-bottom: 4px;
-`
