976c2889f043685f63ad9afaf4ba23cc6ffe8e26
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-03-07 19:49:16 +0900

    ✨ lineClamp 用の style 追加

---
+++ src/styles/lineClamp.ts
@@ -0,0 +1,9 @@
+import { css } from '@emotion/react'
+
+export const lineClamp = (lines: number) => css`
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: ${lines};
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
