4344fb73905dcd49913e8f52b7fdb31c842b48f2
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-03-08 13:17:13 +0900

    🐛 AutoResizeTextarea が ref を受け取れるように

--- src/components/AutoResizeTextarea.tsx
+++ src/components/AutoResizeTextarea.tsx
@@ -1,43 +1,59 @@
 import styled from '@emotion/styled'
-import { ComponentProps, useRef, useState, useCallback, useEffect } from 'react'
+import {
+  ComponentProps,
+  useRef,
+  useState,
+  useCallback,
+  useEffect,
+  forwardRef,
+} from 'react'
 
 type Props = ComponentProps<'textarea'>
-export const AutoResizeTextarea: React.FC<Props> = ({ onChange, ...props }) => {
-  const ref = useRef<HTMLTextAreaElement>(null)
-  const [height, setHeight] = useState<number | null>(null)
-  const [dummyValue, setDummyValue] = useState(props.value ?? '')
+export const AutoResizeTextarea = forwardRef<HTMLTextAreaElement, Props>(
+  ({ onChange, ...props }, receiveRef) => {
+    const ref = useRef<HTMLTextAreaElement>(null)
+    const [height, setHeight] = useState<number | null>(null)
+    const [dummyValue, setDummyValue] = useState(props.value ?? '')
 
-  const onChangeHandler = useCallback(
-    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-      onChange?.(e)
-      setDummyValue(e.target.value)
-    },
-    [onChange]
-  )
-  useEffect(() => {
-    if (ref.current === null) {
-      return
-    }
-    const { scrollHeight } = ref.current
-    setHeight(scrollHeight)
-  }, [dummyValue])
-  useEffect(() => {
-    setDummyValue(props.value ?? '')
-  }, [props.value])
+    const onChangeHandler = useCallback(
+      (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        onChange?.(e)
+        setDummyValue(e.target.value)
+      },
+      [onChange]
+    )
+    useEffect(() => {
+      if (ref.current === null) {
+        return
+      }
+      const { scrollHeight } = ref.current
+      setHeight(scrollHeight)
+    }, [dummyValue])
+    useEffect(() => {
+      setDummyValue(props.value ?? '')
+    }, [props.value])
 
-  return (
-    <>
-      <Textarea height={height} onChange={onChangeHandler} {...props} />
-      <Dummy
-        ref={ref}
-        value={dummyValue}
-        className={props.className}
-        aria-hidden='true'
-        readOnly
-      />
-    </>
-  )
-}
+    return (
+      <>
+        <Textarea
+          ref={receiveRef}
+          height={height}
+          onChange={onChangeHandler}
+          {...props}
+        />
+        <Dummy
+          ref={ref}
+          value={dummyValue}
+          className={props.className}
+          aria-hidden='true'
+          readOnly
+        />
+      </>
+    )
+  }
+)
+AutoResizeTextarea.displayName = 'AutoResizeTextarea'
+
 const Textarea = styled.textarea<{ height: number | null }>`
   height: ${({ height }) => (height === null ? 'auto' : `${height}px`)};
   resize: none;
