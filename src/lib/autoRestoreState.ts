827ab214bb98b75a79a9c8f90f4ee3b672a2dd85
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-03-08 19:28:47 +0900

    ✨ 自動で復帰する useState 的なの作成

---
+++ src/lib/autoRestoreState.ts
@@ -0,0 +1,23 @@
+import { useCallback, useRef, useState } from 'react'
+
+export const useAutoRestoreState = <T>(defaultValue: T, durationMs = 700) => {
+  const [value, setValue] = useState<T>(defaultValue)
+  const timerIdRef = useRef<number>()
+
+  const setValueWillRestore: typeof setValue = useCallback(
+    action => {
+      setValue(action)
+
+      if (timerIdRef.current !== null) {
+        clearTimeout(timerIdRef.current)
+      }
+
+      timerIdRef.current = window.setTimeout(() => {
+        setValue(defaultValue)
+      }, durationMs)
+    },
+    [defaultValue, durationMs]
+  )
+
+  return [value, setValueWillRestore] as const
+}
