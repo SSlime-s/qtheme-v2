76278f8081211938b37dc6bcfd967d8797a0511d
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-03-10 17:33:04 +0900

    ✨ getTheme で version がとれるように

--- src/apollo/resolvers/getTheme.ts
+++ src/apollo/resolvers/getTheme.ts
@@ -3,6 +3,7 @@ import { connectDb } from '@/model/db'
 import { ContextValue } from '.'
 import { QueryResolvers } from '@/apollo/generated/resolvers'
 import { getThemeFromDb } from './utils/getThemeFromDb'
+import { getHistoryFromDb } from './utils/getHistoryFromDb'
 
 export const getTheme: QueryResolvers<ContextValue>['getTheme'] = async (
   _,
@@ -13,17 +14,20 @@ export const getTheme: QueryResolvers<ContextValue>['getTheme'] = async (
   const needCloseConnection = connection === undefined
   try {
     connection = connection ?? (await connectDb())
+    await connection.beginTransaction()
     const theme = await getThemeFromDb(connection, id, userId)
     if (theme === null) {
       throw new GraphQLError('Not found')
     }
+    const history = await getHistoryFromDb(connection, id)
+    await connection.commit()
     return {
       theme,
-      // TODO: あとで実装する
-      versions: [],
+      versions: history,
     }
   } catch (err: unknown) {
     console.error(err)
+    await connection?.rollback()
     if (err instanceof GraphQLError) {
       throw err
     }
