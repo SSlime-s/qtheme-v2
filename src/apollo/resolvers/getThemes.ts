a731433c53a572e600d1b3c053d201efdbad43dd
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-10-08 00:30:52 +0900

    ♻️ only_like の処理を別に分けて getThemes 内を型安全に

--- src/apollo/resolvers/getThemes.ts
+++ src/apollo/resolvers/getThemes.ts
@@ -1,15 +1,13 @@
-import { Prisma } from '@prisma/client'
 import { GraphQLError } from 'graphql'
 import { P, match } from 'ts-pattern'
 
 import type { ContextValue } from '.'
 import type {
   QueryResolvers,
-  Theme,
   Type,
   Visibility,
 } from '@/apollo/generated/resolvers'
-import type { Sql } from '@prisma/client/runtime/library'
+import type { Prisma } from '@prisma/client'
 
 export const getThemes: QueryResolvers<ContextValue>['getThemes'] = async (
   _,
@@ -36,124 +34,170 @@ export const getThemes: QueryResolvers<ContextValue>['getThemes'] = async (
       userId == undefined,
       author == undefined,
     ])
-      .returnType<Sql>()
-      .with([false, P._, P._], () => Prisma.sql`visibility = ${visibility}`)
-      .with([true, true, P._], () => Prisma.sql`visibility = "public"`)
-      .with(
-        [true, false, true],
-        () => Prisma.sql`visibility IN ("public", "private")`
-      )
-      .with(
-        [true, false, false],
-        () =>
-          Prisma.sql`visibility IN ("public", "private")
-          OR
-            (themes.visibility = "draft"
-            AND themes.author_user_id = ${userId})`
-      )
+      .returnType<Prisma.themesWhereInput>()
+      .with([false, P._, P._], () => ({
+        visibility: visibility ?? undefined,
+      }))
+      .with([true, true, P._], () => ({
+        visibility: 'public',
+      }))
+      .with([true, false, true], () => ({
+        visibility: {
+          in: ['public', 'private'],
+        },
+      }))
+      .with([true, false, false], () => ({
+        OR: [
+          {
+            visibility: {
+              in: ['public', 'private'],
+            },
+          },
+          {
+            visibility: 'draft',
+            author_user_id: userId,
+          },
+        ],
+      }))
       .exhaustive()
-    const tableWhereStatement = Prisma.sql`
-        FROM themes
-        LEFT JOIN (
-          SELECT COUNT(*) AS count, theme_id
-          FROM likes
-          GROUP BY theme_id
-        ) AS likes ON likes.theme_id = themes.id
-        ${
-          userId == undefined
-            ? Prisma.empty
-            : Prisma.sql`
-          LEFT JOIN (
-            SELECT theme_id, TRUE AS is_like, created_at
-            FROM likes
-            WHERE user_id = ${userId}
-          ) AS is_like ON is_like.theme_id = themes.id
-        `
-        }
-        WHERE ${visibilityCondition}
-              ${
-                type == undefined
-                  ? Prisma.empty
-                  : Prisma.sql`AND type = ${type}`
-              }
-              ${
-                only_like === true
-                  ? Prisma.sql`AND is_like = TRUE`
-                  : Prisma.empty
-              }
-              ${
-                author == undefined
-                  ? Prisma.empty
-                  : Prisma.sql`AND author_user_id = ${author}`
-              }
-    `
-    const sql = Prisma.sql`
-      SELECT
-        themes.id,
-        themes.title,
-        themes.description,
-        themes.author_user_id,
-        themes.visibility,
-        themes.type,
-        themes.created_at,
-        themes.theme,
-        CASE WHEN likes.count IS NULL THEN 0 ELSE likes.count END AS likes,
-        ${
-          userId == undefined
-            ? Prisma.sql`FALSE`
-            : Prisma.sql`CASE WHEN is_like.is_like = TRUE THEN TRUE ELSE FALSE END`
-        } AS is_like
-        ${tableWhereStatement}
-        ORDER BY ${
-          only_like === true
-            ? Prisma.sql`is_like.created_at DESC`
-            : Prisma.sql`themes.created_at DESC`
-        }
-        ${limit == undefined ? Prisma.empty : Prisma.sql`LIMIT ${limit}`}
-        ${offset == undefined ? Prisma.empty : Prisma.sql`OFFSET ${offset}`}
-    `
+    const mergedThemeCondition: Prisma.themesWhereInput = {
+      ...visibilityCondition,
+      type: type ?? undefined,
+      author_user_id: author ?? undefined,
+    }
+    if (only_like === true) {
+      const themes = await prisma.likes.findMany({
+        select: {
+          themes: {
+            select: {
+              id: true,
+              title: true,
+              description: true,
+              author_user_id: true,
+              visibility: true,
+              type: true,
+              created_at: true,
+              theme: true,
+              _count: {
+                select: {
+                  likes: true,
+                },
+              },
+            },
+          },
+        },
+        where: {
+          user_id: userId,
+          themes: mergedThemeCondition,
+        },
+        orderBy: {
+          created_at: 'desc',
+        },
+        take: limit ?? undefined,
+        skip: offset ?? undefined,
+      })
+      const total = await prisma.likes.count({
+        where: {
+          user_id: userId,
+          themes: mergedThemeCondition,
+        },
+      })
 
-    type Themes = {
-      id: string
-      title: string
-      description: string
-      author_user_id: string
-      visibility: string
-      type: string
-      created_at: Date
-      theme: string
-      likes: bigint
-      is_like: boolean
-    }[]
-    const themes = await prisma.$queryRaw<Themes>(sql)
-    type Count = { count: bigint }[]
-    const [{ count }] = await prisma.$queryRaw<Count>`
-      SELECT COUNT(*) AS count ${tableWhereStatement}
-    `
+      return {
+        themes: themes.map(({ themes }) => {
+          const {
+            id,
+            title,
+            description,
+            author_user_id,
+            visibility,
+            type,
+            created_at,
+            theme,
+            _count: { likes },
+          } = themes
 
-    return {
-      themes: themes.map(theme => {
-        const {
-          created_at,
-          author_user_id,
-          is_like,
-          type,
-          visibility,
-          likes,
-          ...themeRest
-        } = theme
+          return {
+            id,
+            title,
+            description,
+            author: author_user_id,
+            visibility: visibility as Visibility,
+            type: (type ?? 'other') as Type,
+            createdAt: created_at,
+            theme: theme,
+            likes,
+            isLike: true,
+          }
+        }),
+        total,
+      }
+    } else {
+      const themes = await prisma.themes.findMany({
+        select: {
+          id: true,
+          title: true,
+          description: true,
+          author_user_id: true,
+          visibility: true,
+          type: true,
+          created_at: true,
+          theme: true,
+          _count: {
+            select: {
+              likes: true,
+            },
+          },
+          likes: {
+            select: {
+              theme_id: true,
+            },
+            where: {
+              user_id: userId,
+            },
+          },
+        },
+        where: mergedThemeCondition,
+        orderBy: {
+          created_at: 'desc',
+        },
+        take: limit ?? undefined,
+        skip: offset ?? undefined,
+      })
+      const total = await prisma.themes.count({
+        where: mergedThemeCondition,
+      })
 
-        return {
-          ...themeRest,
-          createdAt: created_at,
-          author: author_user_id,
-          isLike: is_like,
-          likes: Number(likes),
-          type: (type ?? 'other') as Type,
-          visibility: visibility as Visibility,
-        }
-      }) satisfies Theme[],
-      total: Number(count),
+      return {
+        themes: themes.map(theme => {
+          const {
+            id,
+            title,
+            description,
+            author_user_id,
+            visibility,
+            type,
+            created_at,
+            theme: theme_,
+            _count: { likes },
+            likes: likes_,
+          } = theme
+
+          return {
+            id,
+            title,
+            description,
+            author: author_user_id,
+            visibility: visibility as Visibility,
+            type: (type ?? 'other') as Type,
+            createdAt: created_at,
+            theme: theme_,
+            likes,
+            isLike: likes_.length === 1,
+          }
+        }),
+        total,
+      }
     }
   } catch (err: unknown) {
     console.error(err)
