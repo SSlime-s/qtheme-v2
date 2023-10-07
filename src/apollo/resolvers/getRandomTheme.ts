84e66bc62f0077c01c8f39cd1d7b76e8ce02afeb
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-10-08 02:01:01 +0900

    ♻️ random theme のところも 2 回クエリを発行するように

--- src/apollo/resolvers/getRandomTheme.ts
+++ src/apollo/resolvers/getRandomTheme.ts
@@ -1,4 +1,5 @@
 import { GraphQLError } from 'graphql'
+import { P, match } from 'ts-pattern'
 
 import type { ContextValue } from '.'
 import type {
@@ -11,7 +12,7 @@ import type { Prisma } from '@prisma/client'
 
 export const getRandomTheme: QueryResolvers<ContextValue>['getRandomTheme'] =
   async (_parent, args, { userId, prisma }) => {
-    const { visibility } = args
+    const { visibility, type } = args
     if (visibility === 'draft') {
       throw new GraphQLError('Invalid visibility')
     }
@@ -20,115 +21,92 @@ export const getRandomTheme: QueryResolvers<ContextValue>['getRandomTheme'] =
     }
 
     try {
-      if (userId === undefined) {
-        const theme = await prisma.$transaction(async prisma => {
-          const { type } = args
+      const visibilityCondition = match([
+        userId === undefined,
+        visibility === undefined || visibility === null,
+      ])
+        .returnType<Prisma.themesWhereInput>()
+        .with([true, P._], () => ({
+          visibility: 'public',
+        }))
+        .with([false, true], () => ({
+          visibility: {
+            in: ['public', 'private'],
+          },
+        }))
+        .with([false, false], () => ({
+          visibility: visibility!,
+        }))
+        .exhaustive()
 
-          const where: Prisma.themesWhereInput = {
-            visibility: 'public',
-            type: type ?? undefined,
-          }
-
-          const themeCount = await prisma.themes.count({
-            where,
-          })
-          const random = Math.floor(Math.random() * themeCount)
-          const theme = await prisma.themes.findFirst({
-            include: {
-              _count: {
-                select: { likes: true },
-              },
-            },
-            where,
-            skip: random,
-          })
-          return theme
-        })
-
-        if (theme === null) {
-          throw new GraphQLError('Not found')
+      const theme = await prisma.$transaction(async prisma => {
+        const where: Prisma.themesWhereInput = {
+          ...visibilityCondition,
+          type: type ?? undefined,
         }
 
-        const {
-          _count: { likes },
-          created_at: createdAt,
-          author_user_id: author,
-          type: type_,
-          visibility,
-          ...themeRest
-        } = theme
-
-        return {
-          ...themeRest,
-          likes,
-          createdAt,
-          author,
-          type: (type_ ?? 'other') as Type,
-          visibility: visibility as Visibility,
-          isLike: false,
-        } satisfies Theme
-      } else {
-        const theme = await prisma.$transaction(async prisma => {
-          // HACK: 外側の visibility を参照しようとすると何故か ReferenceError: before initialization が発生する
-          //       そのため定義し直す
-          const { visibility, type } = args
-          const where: Prisma.themesWhereInput = {
-            visibility:
-              visibility === undefined || visibility === null
-                ? {
-                    in: ['public', 'private'],
-                  }
-                : visibility,
-            type: type ?? undefined,
-          }
-
-          const themeCount = await prisma.themes.count({
-            where,
-          })
-          const random = Math.floor(Math.random() * themeCount)
-          const theme = await prisma.themes.findFirst({
-            include: {
-              _count: {
-                select: { likes: true },
-              },
-              likes: {
-                select: {
-                  theme_id: true,
-                },
-                where: {
-                  user_id: userId,
-                },
+        const themeCount = await prisma.themes.count({
+          where,
+        })
+        const random = Math.floor(Math.random() * themeCount)
+        const theme = await prisma.themes.findFirst({
+          select: {
+            id: true,
+            title: true,
+            description: true,
+            author_user_id: true,
+            visibility: true,
+            type: true,
+            created_at: true,
+            theme: true,
+            _count: {
+              select: {
+                likes: true,
               },
             },
-            where,
-            skip: random,
-          })
-          return theme
+          },
+          where,
+          skip: random,
         })
-        if (theme === null) {
-          throw new GraphQLError('Not found')
-        }
-
-        const {
-          _count: { likes },
-          created_at: createdAt,
-          author_user_id: author,
-          type: type_,
-          visibility,
-          likes: likes_,
-          ...themeRest
-        } = theme
-
-        return {
-          ...themeRest,
-          likes,
-          createdAt,
-          author,
-          type: (type_ ?? 'other') as Type,
-          visibility: visibility as Visibility,
-          isLike: likes_.length !== 0,
-        } satisfies Theme
+        return theme
+      })
+      if (theme === null) {
+        throw new GraphQLError('Not found')
       }
+
+      const getIsLike = async (userId: string): Promise<boolean> => {
+        const like = await prisma.likes.findFirst({
+          select: {
+            theme_id: true,
+          },
+          where: {
+            user_id: userId,
+            theme_id: theme.id,
+          },
+        })
+        return like !== null
+      }
+
+      const is_like = userId === undefined ? false : await getIsLike(userId)
+
+      const {
+        _count: { likes },
+        created_at: createdAt,
+        author_user_id: author,
+        type: type_,
+        visibility: visibility_,
+        ...themeRest
+      } = theme
+
+      return {
+        ...themeRest,
+        likes,
+        createdAt,
+        author,
+        type: (type_ ?? 'other') as Type,
+        visibility: visibility_ as Visibility,
+        isLike: is_like,
+      } satisfies Theme
     } catch (err: unknown) {
       console.error(err)
       if (err instanceof GraphQLError) {
