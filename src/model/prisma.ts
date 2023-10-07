003a6cd30488cd5a945229bddbb5a4104ab66679
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-10-08 02:05:46 +0900

    🗑️ prisma のログ消す

--- src/model/prisma.ts
+++ src/model/prisma.ts
@@ -4,10 +4,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-  })
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
