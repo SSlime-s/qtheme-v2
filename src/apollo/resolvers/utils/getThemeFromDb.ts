898da1885dc9b4b757c74d120aa1bff650139760
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-10-08 01:29:20 +0900

    ♻️ theme 取得するところ queryRaw やめる

--- src/apollo/resolvers/utils/getThemeFromDb.ts
+++ src/apollo/resolvers/utils/getThemeFromDb.ts
@@ -1,86 +1,81 @@
-import { Prisma } from '@prisma/client'
-
 import type { Theme, Type, Visibility } from '@/apollo/generated/resolvers'
-import type { PrismaClient } from '@prisma/client'
+import type { Prisma, PrismaClient } from '@prisma/client'
 
 export const getThemeFromDb = async (
-  prisma: Pick<PrismaClient, '$queryRaw'>,
+  prisma: Pick<PrismaClient, 'themes' | 'likes'>,
   id: string,
   userId?: string
 ) => {
   try {
-    const sql = Prisma.sql`
-    SELECT
-      themes.id,
-      themes.title,
-      themes.description,
-      themes.author_user_id,
-      themes.visibility,
-      themes.type,
-      themes.created_at,
-      themes.theme,
-      CASE WHEN likes.count IS NULL THEN 0 ELSE likes.count END AS likes,
-      ${
-        userId === undefined
-          ? Prisma.sql`FALSE`
-          : Prisma.sql`CASE WHEN is_like.is_like = TRUE THEN TRUE ELSE FALSE END`
-      } AS is_like
-    FROM themes
-    LEFT JOIN (
-      SELECT COUNT(*) AS count, theme_id
-      FROM likes
-      GROUP BY theme_id
-    ) AS likes ON likes.theme_id = themes.id
-    ${
+    const visibilityCondition: Prisma.themesWhereInput =
       userId === undefined
-        ? Prisma.empty
-        : Prisma.sql`
-    LEFT JOIN (
-      SELECT theme_id, TRUE AS is_like
-      FROM likes
-      WHERE user_id = ${userId}
-    ) AS is_like ON is_like.theme_id = themes.id`
-    }
-    WHERE themes.id = ${id} AND ${
-      userId === undefined
-        ? Prisma.sql`visibility = 'public'`
-        : Prisma.sql`(
-        visibility IN ('public', 'private')
-          OR (
-            visibility = 'draft'
-            AND author_user_id = ${userId}
-          )
-      )`
-    }
-    `
+        ? {
+            visibility: 'public',
+          }
+        : {
+            OR: [
+              {
+                visibility: {
+                  in: ['public', 'private'],
+                },
+              },
+              {
+                visibility: 'draft',
+                author_user_id: userId,
+              },
+            ],
+          }
 
-    type Themes = {
-      id: string
-      title: string
-      description: string
-      author_user_id: string
-      visibility: string
-      type: string | null
-      created_at: Date
-      theme: string
-      likes: bigint
-      is_like: boolean
-    }[]
-    const theme = await prisma.$queryRaw<Themes>(sql)
+    const theme = await prisma.themes.findUnique({
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        author_user_id: true,
+        visibility: true,
+        type: true,
+        created_at: true,
+        theme: true,
+        _count: {
+          select: {
+            likes: true,
+          },
+        },
+      },
+      where: {
+        ...visibilityCondition,
+        id,
+      },
+    })
 
-    if (theme.length === 0) {
+    if (theme === null) {
       return null
     }
+
+    const getIsLike = async (userId: string): Promise<boolean> => {
+      const like = await prisma.likes.findFirst({
+        select: {
+          theme_id: true,
+        },
+        where: {
+          user_id: userId,
+          theme_id: id,
+        },
+      })
+
+      return like !== null
+    }
+
+    const is_like = userId === undefined ? false : await getIsLike(userId)
 
     const {
       author_user_id,
       type,
       visibility,
       created_at,
-      is_like,
-      likes,
+      _count: { likes },
       ...restTheme
-    } = theme[0]
+    } = theme
 
     return {
       ...restTheme,
@@ -89,7 +84,7 @@ export const getThemeFromDb = async (
       visibility: visibility as Visibility,
       createdAt: created_at,
       isLike: is_like,
-      likes: Number(likes),
+      likes,
     } satisfies Theme
   } catch (err: unknown) {
     console.error(err)
